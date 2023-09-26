c55c346f284b1acaacbb11e9f395ed8e996ef418
Palldanerar <alexkharin86@gmail.com>
2023-09-26 21:12:58 +0300

    version 2

---
+++ server/controllers/ContactController.js
@@ -0,0 +1,96 @@
+import Contact from "../Models/Contact.js"
+
+export const getAllContacts = async (req, res) => {
+    try {
+
+        const constacts = await Contact.find()
+
+        res.json(constacts)
+
+    } catch(err) {
+        res.json({
+            message: `Ошибка при получении контактов! Ошибка ${err}` 
+        })
+    }
+}
+
+export const getContact = async (req, res) => {
+    try {
+
+        const constact = await Contact.findOne({_id: req.params.id})
+
+        if(!constact) {
+            res.json({
+                message: "Контакта с данным носером не существует!"
+            })
+        }
+
+        res.json(constact)
+
+    } catch(err) {
+        res.json({
+            message: `Ошибка при получении контакта! Ошибка ${err}` 
+        })
+    }
+}
+
+export const createNewContact = async (req, res) => {
+    try {
+
+        const contactPhone = await Contact.findOne({phoneNumber: req.body.phoneNumber})
+
+        if(contactPhone) {
+            return res.json({message: "Данный контакт уже существует!"})
+        }
+
+        const doc = new Contact({
+            name: req.body.name,
+            phoneNumber: req.body.phoneNumber,
+            comment: req.body.comment
+        })
+    
+        await doc.save()
+        res.json({message: "Контакт успешно добавлен!"})
+
+    } catch(err) {
+        res.json({
+            message: `Ошибка при удалении контакта! Ошибка ${err}` 
+        })
+    }
+}
+
+export const updateContact = async (req, res) => {
+    try {
+
+        await Contact.updateOne({
+            _id: req.params.id
+        },{
+            name: req.body.title,
+            phoneNumber: req.body.phoneNumber,
+            comment: req.body.comment,
+        })
+
+        res.json({message: "Контакт успешно обновлён!"})
+
+    } catch(err) {
+        res.json({
+            message: `Ошибка при обновлении контакта! Ошибка ${err}` 
+        })
+    }
+}
+
+export const deleteContact = async (req, res) => {
+    try {
+
+        await Contact.findOneAndDelete({
+            _id: req.params.id
+        })
+
+        res.json({message: "Контакт успешно удалён!"})
+
+    } catch(err) {
+        res.json({
+            message: `Ошибка при удалении контакта! Ошибка ${err}` 
+        })
+    }
+} 
--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import Contact from "./Schema/Contact.js"
 import cors from "cors"
+import { getAllContacts, getContact, createNewContact, updateContact, deleteContact } from './controllers/ContactController.js';
 
 const app = express()
 
@@ -20,66 +20,12 @@ app.get("/", (req, res) => {
     res.send("API для сервиса добавления контактов")
 })
 
-app.get('/contacts', async (req, res) => {
-    try {
-        const users = await Contact.find({});
-        res.json(users);
-    } catch(err) {
-        res.json(err)
-    }
-})
-
-app.post("/contacts", async (req, res) => {
-    try {
-
-        const contactPhone = await Contact.findOne({phoneNumber: req.body.phoneNumber})
-
-        if(contactPhone) {
-            return res.json({status: "FAIL", message: "Данный номер уже существует!"})
-        }
-
-        const doc = new Contact({
-            name: req.body.name,
-            phoneNumber: req.body.phoneNumber,
-            comment: req.body.comment
-        })
-    
-        await doc.save()
-        res.json({status: "OK", message: "Контакт успешно добавлен!"})
-
-    } catch(err) {
-        res.json(err)
-    }
-})
-
-app.patch("/contacts", async (req, res) => {
-    try {
-
-        await Contact.updateOne({
-            phoneNumber: req.body.phoneNumber
-        },{
-            name: req.body.title,
-            comment: req.body.comment,
-        })
-
-        res.json({status: "OK", message: "Контакт успешно обновлён!"})
-
-    } catch(err) {
-        res.json(err)
-    }
-})
-
-app.delete("/contacts", async (req, res) => {
-    try {
-        await Contact.findOneAndDelete({
-            phoneNumber: req.body.phoneNumber
-        })
-
-        res.json({status: "OK", message: "Контакт успешно удалён!"})
-    } catch(err) {
-        res.json(err)
-    }
-})
+// Запросы
+app.get("/contacts", getAllContacts)
+app.get("/contact/:id", getContact)
+app.post("/contact", createNewContact)
+app.patch("/contact/:id", updateContact)
+app.delete("/contact/:id", deleteContact)
 
 app.listen(3100, () => {
   console.log("Server start!")
